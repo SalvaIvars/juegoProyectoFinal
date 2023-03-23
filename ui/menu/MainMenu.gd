f0e96764ec53ef49e534152760c7296c8ad96d78
Salva Ivars Suárez <69164969+SalvaIvars@users.noreply.github.com>
2023-03-23 22:42:17 +0100

    Added menus

---
+++ ui/menu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _on_Level_pressed():
+	get_tree().change_scene("res://ui/menu/LevelMenu.tscn")
